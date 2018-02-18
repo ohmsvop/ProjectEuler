8db12d625efc90fe448482a5129c7c4de59832ce
Yi-An Wu <ohmsvop@gmail.com>
2018-02-18 15:58:25 +0800

    write comment

--- projecteuler89.py
+++ projecteuler89.py
@@ -1,16 +1,19 @@
 f = open("roman.txt")
 saved = 0
 for l in f.readlines():
+    # DCCCC -> CM, CCCC -> CD
     if "DCCCC" in l:
-        saved +=3
+        saved += 3
     elif "CCCC" in l:
-        saved +=2
+        saved += 2
+    # LXXXX -> XC, XXXX -> XL    
     if "LXXXX" in l:
-        saved +=3
+        saved += 3
     elif "XXXX" in l:
-        saved +=2
+        saved += 2
+    # VIIII -> IX, IIII -> IV
     if "VIIII" in l:
-        saved +=3
+        saved += 3
     elif "IIII" in l:
-        saved +=2  
+        saved += 2
 print saved
