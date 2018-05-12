086ba33f6a4a0a4f4b4fca0c57c19598e2181036
Yi-An Wu <ohmsvop@gmail.com>
2018-05-12 14:45:10 +0800

    solved problem 66

--- 66.py
+++ 66.py
@@ -1,20 +1,36 @@
-from __future__ import division
-square = [i**2 for i in range(1,30)]
-D = range(1,100+1)
-for i in square:
-    try:
-        D.remove(i)
-    except:
-        pass
+# Diophantine equation
+from utils import continued_fraction, is_square
+from fractions import Fraction
 
-for i in range(len(square)):
-    for j in range(i):
-        D_rem = (square[i] - 1) % square[j]
-        if (square[i] - 1) % square[j] == 0:
-            print "%d^2 - %d * %d^2 = 1" % (i+1,((square[i] - 1) / square[j]),j+1)
-            try:
-                D.remove(((square[i] - 1) / square[j]))
-            except:
-                pass
-print D
-        
+def approximation(expansion, n):
+    a0 = expansion[0]
+    a1 = expansion[1:]
+    frac = Fraction(0)
+    l = len(expansion) - 1
+    for i in range(n-1, -1, -1):
+        frac = 1/(a1[i%l] + frac)
+    return a0 + frac
+
+def solve_diophantine_equation(D):
+    n = 0
+    expansion = continued_fraction(D)
+    while True:
+        frac = approximation(expansion, n)
+        x = frac.numerator
+        y = frac.denominator
+        if x**2 - D*y**2 == 1:
+            return x, y
+            break
+        n += 1
+
+x_max = 0
+D_max = 0
+for D in range(1000):
+    if is_square(D):
+        continue
+    x,y = solve_diophantine_equation(D)
+    if x > x_max:
+        x_max = x
+        D_max = D
+
+print D_max
